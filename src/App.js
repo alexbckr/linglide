1e535898d8060814f8972fc6168120f05505fdd0
Andrew Wang <andrewxiaoyu0@gmail.com>
2021-03-28 11:35:32 -0400

    Should work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import React, { Component } from "react";
-import Home from './components/Home.js';
-import WelcomeModal from './components/WelcomeModal.js';
-import SetupModal from './components/SetupModal.js';
+import Home from "./components/Home.js";
+import WelcomeModal from "./components/WelcomeModal.js";
+import SetupModal from "./components/SetupModal.js";
 import Loader from "./components/Loader.js";
-import {ReactComponent as Muted} from './images/microphone-alt-slash-solid.svg';
-import {ReactComponent as Unmuted} from './images/microphone-alt-solid.svg';
-import LinglideImage from './images/android-chrome-192x192.png';
+import { ReactComponent as Muted } from "./images/microphone-alt-slash-solid.svg";
+import { ReactComponent as Unmuted } from "./images/microphone-alt-solid.svg";
+import LinglideImage from "./images/android-chrome-192x192.png";
 var io = require("socket.io-client");
 var ss = require("socket.io-stream");
 var RecordRTC = require("recordrtc");
@@ -31,15 +31,13 @@ class App extends Component {
       modalsShown: 0,
       inputLanguage: "",
       outputLanguage: "",
-      inputText:
-        "",
-      outputText:
-        "",
+      inputText: "",
+      outputText: "",
       isMuted: false,
       isDM: false,
       initialized: false,
       socket1: false,
-      socket2: false,
+      socket2: true,
     };
     this.recordAudio = null;
   }
@@ -247,7 +245,8 @@ class App extends Component {
       <div className={this.state.isDM ? "AppDM App" : "App"}>
         <div className={(this.state.isDM ? "headerDM " : "") + "header"}>
           <div onClick={() => this.toggleDM()} className="logo header-item">
-            {/* <img className="real-logo" src={LinglideImage}/> */}<b>Linglide</b>
+            {/* <img className="real-logo" src={LinglideImage}/> */}
+            <b>Linglide</b>
           </div>
           <div onClick={() => this.toggleMute()}>
             {this.state.isMuted ? (
